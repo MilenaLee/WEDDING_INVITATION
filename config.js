52014a782fb3ff10bb9f14fa287baf8be041f43e
Mihyun Lee <mmhyun.lee@gmail.com>
2022-10-20 18:03:55 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ export const WEDDING_INVITATION_URL = "https://minseok-mihyun-wedding.netlify.ap
 export const KAKAOTALK_SHARE_IMAGE =
   "https://lh3.googleusercontent.com/W2-Pauqd6IIc2anFU0WKHE1oJJJ-hNfNIFeJATJJ7ZpggA0ecU_UCzxyh3_u-N6HQDmthbCvllmY-dgoBcL6sCdZDMqvTffmC6oZ9Bdy2_DGqvDP79ytyk7hj-B4l--c6EbU4irV4QzEdlQNb6HnNCem3TG6fXXsXG2wS387B9q-YMyGEZEwfhvZROvx_c7adUWFRnTWqsFvbDKsM4H7Kigc91gETGgah7sZC0kqwYf5IWPZLtJilBINRMrXzV7ITyVOqjZxqrIA4rTFRgRw-9CE57sglnhM1v2klZH_e-AWI1NM3pfdDKNsmmzUmBb8DKsYCHNzHoVTe4k8P0uQb8VG59LJv2djwoem_MoM0IK-r5U8q1x2GOzGMTttYp29J0JU7zOUeGmUU6cem9G7jC10-71Ww-LnpHi2Otl9VsTVCChwfdaAYVAKOi5KKbF6w1dVSGYQcZsRaIWsLASnxDkj36dxtbMhIfd5KfhAL9JF7MbswT0W9Fq_nG-xlsl-Oqd7dQ00MYto2eK_3O0ctfJA_5_l2-9SWeBHnU8kW0iIGEmXeFn656s39n4jkSU4KA88rhb9a4pzQ_4ILmiw8bULgx9-rMkqAZiEtvVTRcO8X9K1c_RZq4nBhfsLOpuO96VN-Ho6SoOqgEXeDFryLTotYVaxfwE1sEAKq1n7QovfGC2h8iAtFEFKhBvrodWuZTPvwllheHtFY9JZoLNA2SChYS0pzBLJrwFahpd1quGj4Q2RhrldzXvNIY8T=w1356-h1694-no?authuser=0";
 
-export const WEDDING_DATE = "2023년 2월 4일 토요일, 오후 2시";
-export const WEDDING_LOCATION = "더컨벤션 영등포, 1층 그랜드볼룸";
+export const WEDDING_DATE = "2023년 2월 4일 토요일 오후 2시";
+export const WEDDING_LOCATION = "더컨벤션 영등포점 1층 그랜드볼룸";
 
 export const GROOM_NAME = "김민석";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
